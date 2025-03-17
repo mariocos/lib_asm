2904c23f657f78e04f6f87da8b00c4ae4971be58
mariocos <alpacasrcool7@gmail.com>
2025-03-17 20:03:08 +0000

    adding my first program written in assembly

---
+++ hello_world/hello_world.asm
@@ -0,0 +1,16 @@
+;rdi, rsi, rdx, r10, r8, and r9 theese are the argument places
+section .data
+	message: db "Hello World!", 0xA
+	message_len: equ 13
+
+section .text
+	global _start
+_start:
+	mov rax, 1; move write syscall into eax
+	mov rdi, 1; specify fd 1
+	mov rsi, message
+	mov rdx, message_len
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
