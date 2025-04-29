1262aae255e559c1b8e6aef18ad7cdaed501da96
mariocos <alpacasrcool7@gmail.com>
2025-04-29 21:51:59 +0100

    adding

--- elf/get_header.c
+++ elf/get_header.c
@@ -32,6 +32,7 @@ void	*get_map(char *str)
 		return (NULL);
 	}
 	off_t f_size = lseek(fd, 0, SEEK_END);
+	printf("the file is of size [%ld]\n", f_size);
 	void	*map = mmap(NULL, f_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	if (map == MAP_FAILED)
 	{
@@ -98,9 +99,38 @@ int get_text_section_index(void *map, Elf64_Ehdr *eheader)
 
 unsigned char shellcode[] = {0xbb, 0x00, 0x00, 0x00, 0x00, 0xb9, 0x00, 0x00, 0x00, 0x00, 0x48, 0x83, 0xfb, 0x0a, 0x7f, 0x0e, 0x48, 0x83, 0xc3, 0x01, 0x48, 0x83, 0xc1, 0x02, 0x48, 0x83, 0xfb, 0x0a, 0x7c, 0xec, 0xb8, 0x3c, 0x00, 0x00, 0x00, 0x48, 0x89, 0xcf, 0x0f, 0x05};
 
+void check_placement_of_header_tables(void *map, Elf64_Ehdr *ehdr)
+{
+	long space_amount;
+	Elf64_Shdr *shdr_table_start = (Elf64_Phdr*)(map + ehdr->e_shoff);
+	Elf64_Shdr *shdrs = (Elf64_Phdr*)(map + ehdr->e_shoff);
+
+	Elf64_Phdr *phdr_table_start  = (Elf64_Phdr*)(map + ehdr->e_phoff);
+	Elf64_Phdr *phdrs = (Elf64_Phdr*)(map + ehdr->e_phoff);
+
+
+	printf("wich header table comes first: [%s]\n",(shdrs < phdrs) ? "sections!" : "programs:(" );
+	printf("yes? [%d]\n",(void*)(&phdrs[ehdr->e_phnum] + 64) - (void*)shdrs);
+	for (short i = 0; i < (short)ehdr->e_shnum; i++)
+	{
+		if (shdrs[i].sh_offset > shdr_table_start || shdrs[i].sh_offset > phdr_table_start)
+		{
+			printf("start of section comes after header tables\n");
+		}
+	}
+	for (short i = 0; i < (short)ehdr->e_phnum; i++)
+	{
+		if (phdrs[i].p_offset > shdr_table_start || phdrs[i].p_offset > phdr_table_start)
+		{
+			printf("start of segment comes after header tables\n");
+		}
+	}
+}
+
+
 int main(void)
 {
-	void *map = get_map("skip");
+	void *map = get_map("a.out");
 
 	print_header(map, 64);
 	Elf64_Ehdr	*header = (Elf64_Ehdr *)map;
@@ -110,8 +140,9 @@ int main(void)
 	int sheader_size = header->e_shentsize;
 
 
-	int text_ind = get_text_section_index(map, header);
-	Elf64_Shdr	*text_sheader = (Elf64_Shdr *)&section_headers[text_ind];
-	printf("text section offset [%p] and size [%d]\n", text_sheader->sh_offset, text_sheader->sh_size);
-	print_header((map + text_sheader->sh_offset), text_sheader->sh_size);
+	//int text_ind = get_text_section_index(map, header);
+	//Elf64_Shdr	*text_sheader = (Elf64_Shdr *)&section_headers[text_ind];
+	check_placement_of_header_tables(map, header);
+
+
 }
